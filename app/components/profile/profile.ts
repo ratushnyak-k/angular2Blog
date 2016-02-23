8b627d5c9ac55e23e72beb61dd4345ba4fe76477
Konstantin Ratushnyak <ratushnyak@steelkiwi.com>
2016-02-23 18:34:03 +0200

    add profile form

--- app/components/profile/profile.ts
+++ app/components/profile/profile.ts
@@ -1,8 +1,21 @@
-import { Component } from 'angular2/core';
+import { Component, Injectable } from 'angular2/core';
 
+@Injectable()
 @Component({
     templateUrl: './app/components/profile/profile.html',
     selector: 'my-profile'
 })
 
-export class MyProfile {}
+
+export class MyProfile {
+    firstName;
+    lastName;
+    email;
+    gender;
+    onSubmit(f, l, e, g):void{
+        console.log(f, l, e, g);
+    }
+    onGenderClick(a):void{
+        console.log(a)
+    }
+}
