aadd868b606ba67de1f7c7d56732220e08fbb952
Konstantin Ratushnyak <ratushnyak@steelkiwi.com>
2016-02-25 16:50:49 +0200

    saving my profile to localstorage

--- app/components/profile/profile.ts
+++ app/components/profile/profile.ts
@@ -11,9 +11,19 @@ export class MyProfile {
 
     myProfileObj: Object = {};
     isEdit:boolean = false;
-
-    toggleEdit(e) {
+    ngOnInit(){
+        if (localStorage.getItem('myInfo')){
+            this.myProfileObj = JSON.parse(localStorage.getItem('myInfo'));
+            this.isEdit = true;
+        }
+    }
+    saveMyProfile(e) {
         e.preventDefault();
-        this.isEdit = !this.isEdit;
+        this.isEdit = true;
+        localStorage.setItem('myInfo', JSON.stringify(this.myProfileObj));
+    }
+    editMyProfile(e) {
+        e.preventDefault();
+        this.isEdit = false;
     }
 }
